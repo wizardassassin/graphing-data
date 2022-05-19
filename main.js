505e11639012faa3a368cbac6f854a166789baa5
wizard_assassin <victorzhai0916@gmail.com>
2022-05-18 23:17:58 -0700

    Functionality and Formatting

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const files = [
         ],
         xAxisTitle: "Years",
         yAxisTitle: "CO2 (parts per million)",
+        data: "",
     },
     {
         file: "data/Global_Temperature.csv",
@@ -25,6 +26,7 @@ const files = [
         yAxes: [{ data: 1 }],
         xAxisTitle: "Years",
         yAxisTitle: "Temperature Anomaly (C)",
+        data: "",
     },
     {
         file: "data/Arctic_Sea_Ice_Extent.csv",
@@ -42,6 +44,7 @@ const files = [
         ],
         xAxisTitle: "Years",
         yAxisTitle: "Million Square km",
+        data: "",
     },
     {
         file: "data/Ocean_Heat_Content.csv",
@@ -59,14 +62,51 @@ const files = [
         ],
         xAxisTitle: "Years",
         yAxisTitle: "Zettajoules",
+        data: "",
     },
 ];
 
+const colors = [
+    "rgb(0, 100, 125, 20)",
+    "rgb(66, 135, 245)",
+    "rgb(235, 64, 52)",
+    "rgb(50, 168, 82)",
+];
+const colorsLen = colors.length;
+
+const template = {
+    file: "data/Arctic_Sea_Ice_Extent.csv",
+    id: "arctic-data",
+    title: "Arctic Sea Ice Extent",
+    type: "bar",
+    xAxis: 0,
+    yAxes: [
+        {
+            data: 5,
+            backgroundColor: "rgb(252, 190, 3)",
+            borderColor: "rgb(252, 190, 3)",
+        },
+        { data: 4 },
+    ],
+    xAxisTitle: "Years",
+    yAxisTitle: "Million Square km",
+    data: "",
+};
+
+const templateString = JSON.stringify(template, null, 4);
+
 const charts = [];
+
+const csvTemplate =
+    "1979,7.05,4.58\n1980,7.67,4.87\n1981,7.14,4.44\n1982,7.3,4.43";
 
 Chart.defaults.color = "blue";
 Chart.defaults.font.size = 16;
 Chart.defaults.font.family = "'Roboto', sans-serif";
+
+document.getElementById("csv-data").placeholder = csvTemplate;
+document.getElementById("json-data").value = templateString;
+document.getElementById("json-data").placeholder = templateString;
 
 (async () => {
     for (const obj of files) {
@@ -74,10 +114,13 @@ Chart.defaults.font.family = "'Roboto', sans-serif";
     }
 })();
 
-async function parseData(obj) {
+async function parseData(obj, prependCanvas) {
     const { file, id, title, type, xAxis, yAxes, xAxisTitle, yAxisTitle } = obj;
-    const res = await fetch(file);
-    const text = await res.text();
+    if (!obj.data) {
+        const res = await fetch(file);
+        obj.data = (await res.text()).trim();
+    }
+    const text = obj.data;
     const arr = text.split("\n").map((x) => x.split(","));
 
     const headers = arr.splice(0, 1).flat();
@@ -86,20 +129,19 @@ async function parseData(obj) {
     const data = {
         labels,
         datasets: yAxes.map(
-            ({ label, backgroundColor, borderColor, data }) => ({
-                label: label ?? headers[data],
-                backgroundColor:
-                    backgroundColor ??
-                    (type == "bar"
-                        ? pattern.draw("dash", "rgb(0, 100, 125, 20)")
-                        : "rgb(0, 100, 125, 20)"),
-                borderColor:
-                    borderColor ??
-                    (type == "bar"
-                        ? pattern.draw("dash", "rgb(0, 100, 125, 20)")
-                        : "rgb(0, 100, 125, 20)"),
-                data: arr.map((x) => x[data]),
-            })
+            ({ label, backgroundColor, borderColor, data }, i) => {
+                const currColor = colors[i % colorsLen];
+                const defaultColor =
+                    type == "bar"
+                        ? pattern.draw("diagonal", currColor)
+                        : currColor;
+                return {
+                    label: label ?? headers[data],
+                    backgroundColor: backgroundColor ?? defaultColor,
+                    borderColor: borderColor ?? defaultColor,
+                    data: arr.map((x) => x[data]),
+                };
+            }
         ),
     };
     const options = {
@@ -125,6 +167,9 @@ async function parseData(obj) {
                 },
                 zoom: {
                     wheel: {
+                        enabled: true,
+                    },
+                    pinch: {
                         enabled: true,
                     },
                     mode: "xy",
@@ -158,6 +203,77 @@ async function parseData(obj) {
     chartCanvas.setAttribute("id", id);
     const myChart = new Chart(chartCanvas, config);
 
-    container.appendChild(chartCanvas);
+    if (prependCanvas) {
+        container.prepend(chartCanvas);
+    } else {
+        container.append(chartCanvas);
+    }
     charts.push(myChart);
+}
+
+function parseInput() {
+    const title = document.getElementById("title").value.trim() || "Epic Title";
+    const xAxisTitle =
+        document.getElementById("xAxis").value.trim() || "X-Axis Title";
+    const yAxisTitle =
+        document.getElementById("yAxis").value.trim() || "Y-Axis Title";
+    const labels =
+        document.getElementById("labels").value.trim() || "year,extent,area";
+    const csvData =
+        document.getElementById("csv-data").value.trim() || csvTemplate;
+    const yAxes = csvData
+        .split("\n")[0]
+        .split(",")
+        .map((x, i) => ({ data: i }))
+        .slice(1);
+    const data = `${labels}\n${csvData}`;
+    const obj = {
+        file: "",
+        id: "",
+        title,
+        type: "line",
+        xAxis: 0,
+        yAxes,
+        xAxisTitle,
+        yAxisTitle,
+        data,
+    };
+    files.push(obj);
+    parseData(obj, true);
+}
+
+function parseInputAdvanced() {
+    const text = document.getElementById("json-data").value || templateString;
+    const obj = JSON.parse(text);
+    files.push(obj);
+    parseData(obj, true);
+}
+
+let isAdvanced = false;
+
+function switchInput() {
+    const simple = document.querySelector("div.simple-container");
+    const advanced = document.querySelector("div.advanced-container");
+    const button = document.getElementById("switch-input");
+    if (isAdvanced) {
+        showDiv(simple);
+        hideDiv(advanced);
+        button.textContent = "Advanced";
+        isAdvanced = false;
+        return;
+    }
+    showDiv(advanced);
+    hideDiv(simple);
+    button.textContent = "Simple";
+    isAdvanced = true;
+}
+
+function hideDiv(div) {
+    div.style.display = "none";
+    div.style.visibility = "hidden";
+}
+
+function showDiv(div) {
+    div.style.display = "block";
+    div.style.visibility = "visible";
 }
